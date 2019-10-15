9a47050b1037978da3bbcfd18a7663eba07522b8
juheon <juheon@juheonucbookair.kr.ibm.com>
2019-10-15 15:13:46 +0900

    init

--- sample-service6/src/main/java/com/koscom/microservices/sample/HelloController.java
+++ sample-service6/src/main/java/com/koscom/microservices/sample/HelloController.java
@@ -6,11 +6,12 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-
+import org.springframework.web.client.RestTemplate;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -30,7 +31,7 @@ public class HelloController {
 	
 	
 	@ApiOperation(value="사용자 정보 가져오기  DB ")
-	@RequestMapping(value="/user/list", method=RequestMethod.GET)
+	@RequestMapping(value="/users", method=RequestMethod.GET)
 	public ResponseEntity <List<SampleUser>> getUserList() { 
 		
 		List<SampleUser> list = null;
@@ -46,9 +47,72 @@ public class HelloController {
 		return new ResponseEntity<List<SampleUser>> (list, HttpStatus.OK);
 	}
 	
+	@ApiOperation(value="사용자 정보 가져오기 2 - TypeAlias ")
+	@RequestMapping(value="/users2", method=RequestMethod.GET)
+	public ResponseEntity <List<SampleUser>> getUserList2() { 
+		
+		List<SampleUser> list = null;
+		try {
+			log.info("Start db select");
+			list = sampleUserDao.selectUser2();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		log.debug("user counts :"+list.size());
+		
+		return new ResponseEntity<List<SampleUser>> (list, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value="사용자 정보 변경하기 ")
+	@RequestMapping(value="/users/{userId}", method=RequestMethod.POST)
+	public ResponseEntity <String > setUserUpdate(
+			@PathVariable(name="userId",required = true ) String userId, 
+			@RequestBody SampleUser sampleUer
+		) throws Exception { 
+		
+		List<SampleUser> list = null;
+		log.info("Start db update");
+		sampleUer.setUserId(userId);
+		int re  = sampleUserDao.updateUser(sampleUer);
+		log.debug("result :"+ re);
+		
+		return new ResponseEntity<String> (re+"", HttpStatus.OK);
+	}
+	
+	@ApiOperation(value="사용자 정보 등록하기 ")
+	@RequestMapping(value="/users/{userId}", method=RequestMethod.PUT)
+	public ResponseEntity <String > setUserInsert(
+			@PathVariable(name="userId",required = true ) String userId, 
+			@RequestBody SampleUser sampleUer
+		) throws Exception { 
+		
+		List<SampleUser> list = null;
+		log.info("Start db insert");
+		sampleUer.setUserId(userId);
+		int re  = sampleUserDao.insertUser(sampleUer);
+		log.debug("result :"+ re);
+		
+		return new ResponseEntity<String> (re+"", HttpStatus.OK);
+	}
+	
+	@ApiOperation(value="사용자 정보 삭하기 ")
+	@RequestMapping(value="/users/{userId}", method=RequestMethod.DELETE)
+	public ResponseEntity <String > setUserDelete(
+			@PathVariable(name="userId",required = true ) String userId
+		) throws Exception { 
+		
+		List<SampleUser> list = null;
+		log.info("Start db insert");
+		int re  = sampleUserDao.deleteUser(userId);
+		log.debug("result :"+ re);
+		
+		return new ResponseEntity<String> (re+"", HttpStatus.OK);
+	}
+	
 	@ApiOperation(value="사용자 정보 가져오기  DB ")
 	@RequestMapping(value="/db/test", method=RequestMethod.GET)
-	public ResponseEntity <String> getUserList2() { 
+	public ResponseEntity <String> getUserListTest() { 
 		
 		int test = -1;
 		try {
@@ -63,4 +127,19 @@ public class HelloController {
 		return new ResponseEntity<String> (test+"", HttpStatus.OK);
 	}	
 
+	
+	@ApiOperation(value="다른 서비스 정보 가져오 ")
+	@RequestMapping(value="/other/service", method=RequestMethod.GET)
+	public ResponseEntity <Object> geOtherService() { 
+		
+		log.info("Start call other services api");
+		
+	    final String uri = "http://localhost:2222/accounts/list";
+	    RestTemplate restTemplate = new RestTemplate();
+	     
+	    Object result = restTemplate.getForObject(uri, Object.class);
+	     
+		
+		return new ResponseEntity<Object> (result, HttpStatus.OK);
+	}	
 }
--- sample-service6/src/main/java/com/koscom/microservices/sample/SampleUserDao.java
+++ sample-service6/src/main/java/com/koscom/microservices/sample/SampleUserDao.java
@@ -13,8 +13,35 @@ public interface SampleUserDao {
 	 * @return
 	 * @throws Exception
 	 */
-	List<SampleUser> selectUser() throws Exception;		
+	List<SampleUser> selectUser() throws Exception;	
 	
-	int selectTest() throws Exception;		
+	List<SampleUser> selectUser2() throws Exception;		
+	
+	/**
+	 * 사용자 정보 변경하
+	 * @param sampleUser
+	 * @return
+	 * @throws Exception
+	 */
+	int updateUser(SampleUser sampleUser) throws Exception;
+	
+	/**
+	 * 사용자 등록하기 
+	 * @param sampleUser
+	 * @return
+	 * @throws Exception
+	 */
+	int insertUser(SampleUser sampleUser) throws Exception;
+	
+	/**
+	 * 사용자 정보 삭제하기 
+	 * @param userId
+	 * @return
+	 * @throws Exception
+	 */
+	int deleteUser(String userId) throws Exception;		
+	
+	int selectTest() throws Exception;
+
 }
 			
